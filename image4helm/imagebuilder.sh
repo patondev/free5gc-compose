32e5e8bc9444ce6d39f1f7d65243b01f0eb7b1e9
Habibie Faried <habibiefaried@gmail.com>
2020-07-14 22:45:15 +0000

    adding sh

---
+++ image4helm/imagebuilder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker build . -t habibiefaried/free5gc:Amf -f DockerfileAmf
+docker build . -t habibiefaried/free5gc:Ausf -f DockerfileAusf
+docker build . -t habibiefaried/free5gc:N3iwf -f DockerfileN3iwf
+docker build . -t habibiefaried/free5gc:Nrf -f DockerfileNrf
+docker build . -t habibiefaried/free5gc:Nssf -f DockerfileNssf
+docker build . -t habibiefaried/free5gc:Pcf -f DockerfilePcf
+docker build . -t habibiefaried/free5gc:Smf -f DockerfileSmf
+docker build . -t habibiefaried/free5gc:Udm -f DockerfileUdm
+docker build . -t habibiefaried/free5gc:WebUI -f DockerfileWebUI
